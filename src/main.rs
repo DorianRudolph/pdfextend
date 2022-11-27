32bb0fd9b7ac662657d13b488ac0761692de6791
Dorian Rudolph <mail@dorianrudolph.com>
2022-11-27 18:56:01 +0100

    add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use pdfium_render::prelude::*;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -10,8 +11,18 @@ struct Args {
     output: String,
 }
 
+fn extend_pdf(input: &str, output: &str) -> Result<(), PdfiumError> {
+    let pdfium = Pdfium::new(
+        Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path("./"))
+            .or_else(|_| Pdfium::bind_to_system_library())?,
+    );
+    let doc = pdfium.load_pdf_from_file(input, None)?;
+    Ok(())
+}
+
 fn main() {
     let args = Args::parse();
 
     println!("Input: {}\nOutput: {}", args.input, args.output);
+    extend_pdf(&args.input, &args.output).unwrap()
 }
