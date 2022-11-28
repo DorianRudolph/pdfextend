ff50850a351ef41632cb878cd97fadc45f215876
Dorian Rudolph <mail@dorianrudolph.com>
2022-11-28 12:56:23 +0100

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use pdfium_render::prelude::*;
+use std::{env, path::PathBuf};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -17,10 +18,25 @@ fn extend_pdf(input: &str, output: &str) -> Result<(), PdfiumError> {
             .or_else(|_| Pdfium::bind_to_system_library())?,
     );
     let doc = pdfium.load_pdf_from_file(input, None)?;
+    for mut page in doc.pages().iter() {
+        let bounds = page.boundaries_mut();
+        let mut rect = bounds.crop().unwrap().bounds;
+        // rect.left -= PdfPoints::from_mm(50.);
+        // println!("{:?}", page.boundaries().media()?);
+        // println!("{:?}", page.boundaries().media()?);
+        // println!();
+    }
     Ok(())
 }
 
+fn current_path() -> Option<PathBuf> {
+    Some(env::current_exe().ok()?.parent()?.to_owned())
+}
+
 fn main() {
+    let path = current_path().unwrap_or("./".into());
+    println!("path: {}", path.display());
+    println!("path2: {}", Pdfium::pdfium_platform_library_name_at_path(path.display()));
     let args = Args::parse();
 
     println!("Input: {}\nOutput: {}", args.input, args.output);
